d14621393cb57c6486d7a29c2ddc617a300c5b90
Ashutosh Singh <ashutosh.singh@inferworks.com>
2026-05-18 10:07:29 +0530

    update

---
+++ namasteReact_Revison/src/component/Error.js
@@ -0,0 +1,16 @@
+import { useRouteError } from "react-router-dom";
+
+const Error=()=>{
+    const err=useRouteError();
+    console.log(err)
+    return (
+        <div>
+            <h1>Opps !!!</h1>
+            <h1>some thing went wrong</h1>
+            <h2>{err.status}:{err.statusText}</h2>
+            <h2>{err.error.message}</h2>
+        </div>
+    )
+}
+
+export default Error;
